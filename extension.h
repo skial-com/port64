8a3f3ec12c7d8399d4d96d24fa44082bbc076b4b
Kenzzer <kenzzer@users.noreply.github.com>
2024-06-08 12:16:12 +0200

    Drop metamod, update buildscript

--- extension.h
+++ extension.h
@@ -81,39 +81,6 @@ public:
 	 * @return			True if working, false otherwise.
 	 */
 	//virtual bool QueryRunning(char *error, size_t maxlen);
-public:
-#if defined SMEXT_CONF_METAMOD
-	/**
-	 * @brief Called when Metamod is attached, before the extension version is called.
-	 *
-	 * @param error			Error buffer.
-	 * @param maxlen		Maximum size of error buffer.
-	 * @param late			Whether or not Metamod considers this a late load.
-	 * @return				True to succeed, false to fail.
-	 */
-	virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool late);
-
-	/**
-	 * @brief Called when Metamod is detaching, after the extension version is called.
-	 * NOTE: By default this is blocked unless sent from SourceMod.
-	 *
-	 * @param error			Error buffer.
-	 * @param maxlen		Maximum size of error buffer.
-	 * @return				True to succeed, false to fail.
-	 */
-	//virtual bool SDK_OnMetamodUnload(char *error, size_t maxlen);
-
-	/**
-	 * @brief Called when Metamod's pause state is changing.
-	 * NOTE: By default this is blocked unless sent from SourceMod.
-	 *
-	 * @param paused		Pause state being set.
-	 * @param error			Error buffer.
-	 * @param maxlen		Maximum size of error buffer.
-	 * @return				True to succeed, false to fail.
-	 */
-	//virtual bool SDK_OnMetamodPauseChange(bool paused, char *error, size_t maxlen);
-#endif
 };
 
 #endif // _INCLUDE_SOURCEMOD_EXTENSION_PROPER_H_
